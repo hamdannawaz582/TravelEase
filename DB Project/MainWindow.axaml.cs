16292a9fcea072a6fe5f0290da9aed40131cafe0
Hamdan Nawaz <hamdannawaz582@gmail.com>
2025-04-26 22:11:58 +0500

    Added ability to log in as different types of users, current login page denoted by label at top left.

--- DB Project/MainWindow.axaml.cs
+++ DB Project/MainWindow.axaml.cs
@@ -1,4 +1,9 @@
 using Avalonia.Controls;
+using System;
+using Avalonia;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace DB_Project;
 
@@ -7,5 +12,59 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = this;
+        EntryText.Text = "Traveller Entry";
+    }
+
+    public String Entry = "Traveller";
+
+    public String UserName;
+    public String Password;
+
+    private void Login_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Login.Background = Brushes.Red;
+        Console.WriteLine($"Name: {UserName}");
+        Console.WriteLine($"Password: {Password}");
+        Console.WriteLine($"Login: {Entry}");
+    }
+
+    private void UsernameBox_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        UserName = UsernameBox.Text;
+    }
+
+    private void PasswordBox_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        Password = PasswordBox.Text;
+    }
+
+    private void Signup_OnClick(object? sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void TravellerButton_OnTapped(object? sender, TappedEventArgs e)
+    {
+        Entry = "Traveller";
+        EntryText.Text = $"{Entry} Entry";
+    }
+
+    private void OperatorButton_OnTapped(object? sender, TappedEventArgs e)
+    {
+        Entry = "Operator";
+        EntryText.Text = $"{Entry} Entry";
+    }
+
+    private void HotelButton_OnTapped(object? sender, TappedEventArgs e)
+    {
+        Entry = "Hotel";
+        EntryText.Text = $"{Entry} Entry";
+    }
+
+    private void AdminButton_OnTapped(object? sender, TappedEventArgs e)
+    {
+        Entry = "Admin";
+        EntryText.Text = $"{Entry} Entry";
     }
 }
