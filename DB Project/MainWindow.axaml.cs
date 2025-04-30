3dafe16299e3692d365c4a861da6202a05cb07f9
Saim Sarfraz <saim.sarfraz@outlook.com>
2025-05-01 01:35:33 +0500

    Stuff

--- DB Project/MainWindow.axaml.cs
+++ DB Project/MainWindow.axaml.cs
@@ -12,7 +12,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        MainContent.Content = new LoginPage();
+        MainContent.Content = new CreateEditGenericWindow();
     }
 
     
