c9f315459d2ce3b65bd98e278146d8a9a75454fc
Saim Sarfraz <saim.sarfraz@outlook.com>
2025-05-04 12:10:10 +0500

    Simplified BaseShell.cs

--- DB Project/BaseShell.cs
+++ DB Project/BaseShell.cs
@@ -5,36 +5,24 @@ namespace DB_Project
 {
     public abstract class BaseShell : UserControl
     {
-        // Removed fields: SplitView, Sidebar, MainContent
         protected string Username;
 
         public BaseShell(string username)
         {
             Username = username;
         }
-
-        // Modified to accept controls as parameters
+        
         protected void InitializeSidebar(SplitView splitView, SidebarControl sidebar, ContentControl mainContent)
         {
-            // Add null checks for parameters
-            if (splitView == null)
-                throw new ArgumentNullException(nameof(splitView), "SplitView cannot be null when initializing sidebar");
-            if (sidebar == null)
-                throw new ArgumentNullException(nameof(sidebar), "Sidebar cannot be null when initializing sidebar");
-            if (mainContent == null)
-                throw new ArgumentNullException(nameof(mainContent), "MainContent cannot be null when initializing sidebar");
-
-            // These need to be set in this order
             sidebar.SetSplitView(splitView);
             sidebar.SetPageHost(mainContent);
         }
-
-        // Modified to accept sidebar as a parameter
+        //Add common tabs shared between all shells here,, the login page is just added for testing
         protected void ConfigureCommonTabs(SidebarControl sidebar)
         {
             if (sidebar == null)
                 throw new ArgumentNullException(nameof(sidebar), "Sidebar cannot be null when configuring common tabs");
-
+            
             sidebar.AddTab("Login Page", new LoginPage());
         }
     }
