9c5c92a795633128a40d65917fe399de26cd1ec1
Saim Sarfraz <saim.sarfraz@outlook.com>
2025-05-05 21:42:19 +0500

    CategoryManagement added to Admin SideBar

--- DB Project/AdminPages/AdminShell.axaml.cs
+++ DB Project/AdminPages/AdminShell.axaml.cs
@@ -30,9 +30,11 @@ namespace DB_Project
                     InitializeSidebar(splitView, sidebar, mainContent);
                     var analyticsPage = new AdminAnalytics();
                     var managementPage = new AdminManagement();
+                    var catergoryManagement = new CategoryManagement();
                     var reviewsPage = new AdminReviews();
                     Sidebar.AddTab("Analytics", analyticsPage);
-                    Sidebar.AddTab("Management", managementPage);
+                    Sidebar.AddTab("Admin Management", managementPage);
+                    Sidebar.AddTab("Category Management", catergoryManagement);
                     Sidebar.AddTab("Reviews", reviewsPage);
                     ConfigureCommonTabs(sidebar);
                     MainContent.Content = analyticsPage;
