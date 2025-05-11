b09d39ed8a618a4d13b854bd6c18874e8a8d3aa1
Areeb <areebfaisal49@gmail.com>
2025-05-12 02:01:53 +0500

    Added Hotel Drop Down Menu

--- DB Project/CompanyPages/CompanyHotel.axaml.cs
+++ DB Project/CompanyPages/CompanyHotel.axaml.cs
@@ -1,42 +1,98 @@
+using System;
+using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System.Collections.ObjectModel;
+using DB_Project.Services;
+using Microsoft.Data.SqlClient;
 
 namespace DB_Project.CompanyPages
 {
     public partial class CompanyHotel : UserControl
     {
         public ObservableCollection<Assignment> Assignments { get; set; }
-
-        public CompanyHotel()
+        string OperatorUsername;
+        public CompanyHotel(string OpUsername)
         {
             InitializeComponent();
             DataContext = this;
+            OperatorUsername = OpUsername;
             LoadAssignments();
             AssignmentsContainer.ItemsSource = Assignments;
         }
-
+        
         private void LoadAssignments()
         {
-            Assignments = new ObservableCollection<Assignment>
+            Assignments = new ObservableCollection<Assignment>();
+
+            string bookingQuery = @"
+                SELECT tb.Username, t.Title, t.TripID
+                FROM Trip_Booking tb
+                JOIN Trip t ON tb.TripID = t.TripID
+                WHERE t.OperatorUsername = @OperatorUsername";
+
+            string hotelQuery = "SELECT HUsername FROM Trip_Hotels WHERE TripID = @TripID";
+
+            try
             {
-                new Assignment
+                using (var connection = DatabaseService.Instance.CreateConnection())
+                using (var bookingCommand = new SqlCommand(bookingQuery, connection))
                 {
-                    Username = "john_doe",
-                    TripTitle = "Summer Vacation",
-                    Hotel = "Grand Plaza Hotel",
-                    Service = "Airport Transfer"
-                },
-                new Assignment
-                {
-                    Username = "jane_smith",
-                    TripTitle = "Business Conference",
-                    Hotel = "Seaside Resort",
-                    Service = "Daily Breakfast"
+                    bookingCommand.Parameters.AddWithValue("@OperatorUsername", OperatorUsername);
+                    connection.Open();
+
+                    using (var bookingReader = bookingCommand.ExecuteReader())
+                    {
+                        while (bookingReader.Read())
+                        {
+                            string tripID = bookingReader["TripID"].ToString();
+                            var hotels = new List<string>();
+
+                            using (var hotelConnection = DatabaseService.Instance.CreateConnection())
+                            using (var hotelCommand = new SqlCommand(hotelQuery, hotelConnection))
+                            {
+                                hotelCommand.Parameters.AddWithValue("@TripID", tripID);
+                                hotelConnection.Open();
+
+                                using (var hotelReader = hotelCommand.ExecuteReader())
+                                {
+                                    while (hotelReader.Read())
+                                    {
+                                        hotels.Add(hotelReader["HUsername"].ToString());
+                                        
+                                    }
+                                }
+                            }
+                            hotels.Add("None");
+
+                            Assignments.Add(new Assignment
+                            {
+                                Username = bookingReader["Username"].ToString(),
+                                TripTitle = bookingReader["Title"].ToString(),
+                                TripID = tripID,
+                                Hotel = "",
+                                Service = "",
+                                AvailableHotels = new ObservableCollection<string>(hotels)
+                            });
+                        }
+                    }
+
+                    AssignmentsContainer.ItemsSource = Assignments;
                 }
-            };
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Error loading assignments: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"General error loading assignments: {ex.Message}");
+            }
         }
 
+
+
+        
         private void OnAssignClick(object sender, RoutedEventArgs e)
         {
             if (sender is Button button && button.Tag is Assignment assignment)
@@ -52,5 +108,8 @@ namespace DB_Project.CompanyPages
         public string TripTitle { get; set; }
         public string Hotel { get; set; }
         public string Service { get; set; }
+        public string TripID { get; set; }
+        public ObservableCollection<string> AvailableHotels { get; set; }
+
     }
 }
